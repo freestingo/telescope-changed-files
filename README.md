bfa92c1133c2f3bd361c5d7d57fdae6ac870d44f
Nicolo Traini <nicolo.traini@oncode.it>
2022-05-22 23:00:10 +0200

    Use builtin git_status previewer when selecting current branch

--- README.md
+++ README.md
@@ -34,4 +34,4 @@ This will add a custom mapping only active inside the `git_branches` builtin Tel
 # General notes
 It should be obvious, even after a quick glance at the source code, that I don't really know what the hell I'm doing here.
 This is my first ever project in lua, but I can already see some additions that would be good for this project (i.e. better and fancier filename viewing).
-As fun as writing this code was, I have neither the time nor the inclination to give it that much thought after the exact moment I will press "Commit changes" (aka 30 seconds from now). So, needless to say PRs are very welcome. 
+As fun as writing this code was, I have neither the time nor the inclination to give it any more thought starting from the very moment I will press "Commit changes" (aka 30 seconds from now). So, needless to say PRs are very welcome.
--- lua/telescope/_extensions/changed-files/actions.lua
+++ lua/telescope/_extensions/changed-files/actions.lua
@@ -2,9 +2,30 @@ local action_state = require "telescope.actions.state"
 local conf = require("telescope.config").values
 local finders = require "telescope.finders"
 local pickers = require "telescope.pickers"
+local previewers = require "telescope.previewers"
 local custom_previewers = require "telescope._extensions.changed-files.previewers"
 
 local M = {}
+
+local get_current_branch = function()
+  local handle = io.popen("git branch --show-current")
+  local result = handle:read("*a")
+  handle:close()
+
+  -- TODO: this next for-loop relies on the above command only returning one line we actually care about.
+  -- there's gotta be a better way of extracting the branch name from the result
+  for token in string.gmatch(result, "[^%s]+") do
+    return token
+  end
+end
+
+local get_previewer = function(current_branch, selected_branch, opts)
+  if current_branch == selected_branch then
+    return previewers.git_file_diff.new(opts) -- use builtin previewer (same as for `:Telescope git_status`)
+  else
+    return custom_previewers.git_file_diff_from_branch.new({ branch_name = selected_branch }) -- use custom previewer
+  end
+end
 
 M.find_changed_files = function(prompt_bufnr)
   local branch_name = action_state.get_selected_entry().value
@@ -19,9 +40,10 @@ M.find_changed_files = function(prompt_bufnr)
 	end
 
   local opts = {} -- TODO: find a way to pass these in
+  local previewer = get_previewer(get_current_branch(), branch_name, opts)
 	pickers.new(opts, {
 		prompt_title = "Changed files since " .. branch_name,
-    previewer = custom_previewers.git_file_diff_from_branch.new({ branch_name = branch_name }),
+    previewer = previewer,
 		finder = finders.new_table {
 			results = files
 		},
@@ -30,4 +52,3 @@ M.find_changed_files = function(prompt_bufnr)
 end
 
 return M
-
